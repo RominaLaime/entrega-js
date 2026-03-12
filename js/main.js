1a89dd20201d69da2bebd38bdc6aec60e7100e1c
RominaLaime <roomina.laime@gmail.com>
2026-03-12 02:10:41 -0300

    JS actualizado

--- js/main.js
+++ js/main.js
@@ -1,69 +1,126 @@
 //Saludo
+let botonSaludo = document.getElementById("saludarBtn");
 
-function pedirNombre() {
-    let nombre = prompt("Ingresa tu nombre:");
+    botonSaludo.addEventListener("click", function(){
 
-    while (!isNaN(nombre) || nombre === ""){
-        alert("Error, por favor, ingrese un nombre válido (solo letras)");
-        nombre = prompt("Ingresa tu nombre:");
-    }
-    alert("Hola " +  nombre + ", bienvenid@ a nuestra tienda online de moda :)")
-}
+        let nombre = document.getElementById("nombreInput").value;
 
-pedirNombre();
+        if(!isNaN(nombre) || nombre === ""){
+            mensaje.innerText = "Por favor ingresá un nombre válido";
+            return;
+        }
 
+        mensaje.innerText = "";
+
+        document.getElementById("saludo").innerText =
+        "Hola " + nombre + ", bienvenid@ a nuestra tienda online de moda.";
+    });
+
+//Mensaje para mostrar error o avisos
+    let mensaje = document.getElementById("mensaje");
 
 //Productos
-let ropa = ["remera","pantalon","buzo"];
+let ropa = [
+    {nombre:"remera", precio:15000},
+    {nombre:"pantalon", precio:30000},
+    {nombre:"buzo", precio:40000}
+];
 
-const precios = {
-    remera: 15000,
-    pantalon: 30000,
-    buzo: 40000
-};
 
-let carrito = [];
-let total = 0;
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-function mostrarProductos(){
-    alert("Productos disponibles:\n- " + ropa. join("\n- "));
+
+//Muestro productos
+let contenedorProductos = document.getElementById("productos");
+
+ropa.forEach(producto => {
+
+    let div = document.createElement("div");
+
+    div.innerHTML = `
+        <p>${producto.nombre} - $${producto.precio}</p>
+        <input type="number" min="1" placeholder="Cantidad" id="cantidad-${producto.nombre}">
+        <button>Agregar al carrito</button>
+    `;
+
+    div.querySelector("button").addEventListener("click", function(){
+
+        let cantidadInput = document.getElementById(`cantidad-${producto.nombre}`).value;
+        let cantidad = parseInt(cantidadInput);
+
+        if(isNaN(cantidad) || cantidad <= 0){
+            mensaje.innerText = "Ingresá una cantidad válida.";
+            return;
+        }
+
+        mensaje.innerText = "";
+
+        agregarAlCarrito(producto, cantidad);
+    });
+
+    contenedorProductos.appendChild(div);
+
+});
+
+//Agrego producto/s al carrito
+    function agregarAlCarrito(producto, cantidad){
+
+    carrito.push({
+        producto: producto.nombre,
+        precio: producto.precio,
+        cantidad: cantidad
+    });
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+
+    mostrarCarrito();
 }
 
 
-//Compra
-function iniciarCompra() {
 
-    let continuar = confirm("¿Queres ver los productos disponibles?");
+//Compra - Carrito
+function mostrarCarrito(){
 
-    
-if (!continuar) {
-    alert("Gracias por visitarnos.")
-    return;
+    let listaCarrito = document.getElementById("carrito");
+
+    listaCarrito.innerHTML = "";
+
+    let total = 0;
+
+    carrito.forEach((item, index) => {
+
+        let li = document.createElement("li");
+
+        let subtotal = item.precio * item.cantidad;
+
+        li.innerText =
+        item.producto + " x" + item.cantidad + " - $" + subtotal;
+
+        let botonEliminar = document.createElement("button");
+        botonEliminar.innerText = "Eliminar";
+
+        botonEliminar.addEventListener("click", function(){
+
+            carrito.splice(index,1);
+
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+
+            mostrarCarrito();
+
+        });
+
+        li.appendChild(botonEliminar);
+
+        listaCarrito.appendChild(li);
+
+        total += subtotal;
+
+    });
+
+    document.getElementById("total").innerText =
+    "Total de la compra: $" + total;
 }
 
-while (continuar){
-    mostrarProductos();
-    let productoElegido = prompt ("Escribí el nombre del producto que queres comprar:");
-        if (precios[productoElegido]) {
-            let cantidad = parseInt (prompt ("¿Cuantas unidades querés comprar?"));
-            let subtotal = precios[productoElegido] * cantidad;
 
-            carrito.push(productoElegido);
-            total += subtotal;
-       
+mostrarCarrito();
 
-            alert("Agregaste " + cantidad + " " + productoElegido + " al carrito");
-            console.log("Carrito actual:", carrito); 
-    
-    }else{
-        alert("Producto inválido");
-    }
-    continuar = confirm("¿Queres seguir comprando?");
-}
- alert("el total de tu compra es de: $" + total);
- console.log("Total final", total)
- alert("Gracias por elegirnos ;)")   
-
-}
-
-iniciarCompra();
